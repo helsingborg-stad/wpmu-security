54316c63067228aa55837728827461cf6b3615b9
GitHub Action <>
2025-05-27 14:52:16 +0000

    build: update package file versions to 1.0.1

--- wpmu-security.php
+++ wpmu-security.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name:    WPMU Security
  * Description:    Adds basic security features to WordPress.
- * Version: 0.6.4
+ * Version: 1.0.1
  * Author:         Sebastian Thulin
  * Author URI:     https://github.com/helsingborg-stad
  * License:        MIT
